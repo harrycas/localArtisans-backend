f73014ab9d052fbe01a9bf7115bdbfa3ad1d8672
harrycas <hcastror1@upao.edu.pe>
2024-06-30 12:24:23 -0500

    ConfigMap & Secret

--- msvc-catalog/src/main/java/com/arquiproject/msvc_catalog/client/UserClientRest.java
+++ msvc-catalog/src/main/java/com/arquiproject/msvc_catalog/client/UserClientRest.java
@@ -2,6 +2,6 @@ package com.arquiproject.msvc_catalog.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "svc-artisans")
+@FeignClient(name = "svc-artisans", url = "${svc.artisans.url}")
 public interface UserClientRest {
 }
--- svc-artisans/src/main/java/com/arquiproject/svc_artisans/client/CatalogClientRest.java
+++ svc-artisans/src/main/java/com/arquiproject/svc_artisans/client/CatalogClientRest.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-catalog")
+@FeignClient(name = "msvc-catalog", url = "${msvc.catalog.url}")
 public interface CatalogClientRest {
 
     @PutMapping("/update/userDelete/{userId}")
     void updateProductsToInactive(@PathVariable int userId);
 
 }
+
+//@FeignClient(name = "msvc-catalog", url = "${msvc.catalog.url}")
+//When still using the url from app properties to connect with other microservice
